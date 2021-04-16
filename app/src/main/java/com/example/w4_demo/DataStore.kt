a4b76112c1c62a15dfa9ce19ccdf493ab020a554
Nam Pham <nam.phho@gmail.com>
2021-04-16 23:46:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/w4_demo/DataStore.kt
@@ -0,0 +1,19 @@
+package com.example.w4_demo
+
+/**
+ * Created by nampham on 4/16/21.
+ */
+
+fun getDataSet(): List<Idol> {
+    return listOf(
+        Idol("Nguyen Quang Hai", "Footballer", R.drawable.quanghai),
+        Idol("Bui Tien Dung", "Footballer", R.drawable.buitiendung),
+        Idol("Duy Manh", "Footballer", R.drawable.duymanh),
+        Idol("Cong Phuong", "Footballer", R.drawable.congphuong),
+        Idol("Van Toan", "Footballer", R.drawable.vantoan),
+        Idol("Huynh Duc", "Football Coach", R.drawable.huynhduc),
+        Idol("Vinh Rau", "Comedian", R.drawable.vinhrau),
+        Idol("Cong Vinh", "Football Manager", R.drawable.conhvinh),
+        Idol("Dinh Manh Ninh", "Artist", R.drawable.dmninh)
+    )
+}
---
+++ app/src/main/java/com/example/w4_demo/Idol.kt
@@ -0,0 +1,7 @@
+package com.example.w4_demo
+
+/**
+ * Created by nampham on 4/16/21.
+ */
+class Idol(val name: String, val job: String, val avatar: Int) {
+}
